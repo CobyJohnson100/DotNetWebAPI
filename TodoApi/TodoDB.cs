595c5bcab09a36d491eae9a78753b86b2ea718ce
cobyfireshark <coby@fireshark.com>
2024-03-29 11:55:00 -0500

    tutorial finished

---
+++ TodoApi/TodoDB.cs
@@ -0,0 +1,10 @@
+// TodoAPI/TodoDB.cs
+using Microsoft.EntityFrameworkCore;
+
+class TodoDb : DbContext
+{
+    public TodoDb(DbContextOptions<TodoDb> options)
+        : base(options) { }
+
+    public DbSet<Todo> Todos => Set<Todo>();
+}
